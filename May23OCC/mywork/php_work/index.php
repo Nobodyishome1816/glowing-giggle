5cb5cee76aa119a8ffd8ee24fef9051cac08afde
Nobodyishome1816 <tommcallister1816@gmail.com>
2025-01-30 14:35:14 +0000

    starting to create website

---
+++ May23OCC/mywork/php_work/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>Health Advice Group</title>
+</head>
+<body>
+    <div class="main">
+        <p>
+            welcome to the website
+        </p>
+        <div id="navbar">
+            <ul>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
